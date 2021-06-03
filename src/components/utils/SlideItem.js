78c1096136c244cd15b7025142a64b28bbcc2133
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2021-06-03 16:34:14 +0200

    3rd party component try

--- src/components/utils/SlideItem.js
+++ src/components/utils/SlideItem.js
@@ -1,31 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo1Pic from "../../images/logo 1.png";
 import Logo2Pic from "../../images/logo 2.png";
 import Logo3Pic from "../../images/logo 3.png";
 import Logo4Pic from "../../images/logo 4.png";
 
 export default function SlideItem(props) {
-  const logoHandler = (key) => {
-    switch (key) {
+  const { block, id } = props;
+  const [logo, setLogo] = useState("../../images/logo 1.png");
+  console.log(id);
+  useEffect(() => {
+    switch (id) {
       case "1":
-        return <Logo1Pic />;
+        return setLogo(Logo1Pic);
       case "2":
-        return Logo2Pic;
+        return setLogo(Logo2Pic);
       case "3":
-        return Logo3Pic;
+        return setLogo(Logo3Pic);
       case "4":
-        return Logo4Pic;
-
+        return setLogo(Logo4Pic);
       default:
         break;
     }
-  };
-  const { block, key } = props;
-  return (
-    <img
-      className={`${block}__slide`}
-      src={logoHandler(key)}
-      alt="company logo"
-    />
-  );
+  }, [id]);
+
+  return <img className={`${block}__slide`} src={logo} alt="company logo" />;
 }
--- src/components/utils/Slideshow.js
+++ src/components/utils/Slideshow.js
@@ -6,35 +6,21 @@ import Item from "./SlideItem";
 export default function Slideshow(props) {
   const { block } = props;
 
-  const [currentItem, setCurrentItem] = useState(0);
-
-  const nextItem = () => {
-    const newIndex = currentItem + 1;
-    setCurrentItem(newIndex);
-  };
-  const prevItem = () => {
-    const newIndex = currentItem - 1;
-    setCurrentItem(newIndex);
-  };
   const items = [1, 2, 3, 4];
 
   return (
     <aside className={`${block}__slideshowContainer`}>
       <div className={`${block}__slideshow`}>
         {items.map((item, i) => (
-          <Item block={block} key={i} />
+          <Item block={block} key={i} id={i} />
         ))}
-
-        {/* <img className={`${block}__slide`} src={Logo2Pic} alt="company logo" />
-        <img className={`${block}__slide`} src={Logo3Pic} alt="company logo" />
-        <img className={`${block}__slide`} src={Logo4Pic} alt="company logo" /> */}
       </div>
-      <button onClick={prevItem} className={`${block}__slideLeft`}>
-        <img src={arrow} alt="arrow left icon" />
-      </button>
-      <button onClick={nextItem} className={`${block}__slideRight`}>
+      {/* <button onClick={prevItem} className={`${block}__slideLeft`}> */}
+      {/* <img src={arrow} alt="arrow left icon" />
+      </button> */}
+      {/* <button onClick={nextItem} className={`${block}__slideRight`}>
         <img src={arrow} alt="arrow righ icon" />
-      </button>
+      </button> */}
     </aside>
   );
 }
